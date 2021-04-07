8e6272c2c00d7a5590fcbd7cafe72534df1bdb7b
Hatem Ragab <37384769+hatemragab@users.noreply.github.com>
2021-04-07 08:38:19 +0200

    test 2

--- bin/main.dart
+++ bin/main.dart
@@ -1,22 +1,17 @@
-import 'package:alfred/alfred.dart';
+
 import 'package:palace/palace.dart';
 import 'package:public_e_commerce_api_dart/mysql/db_provider.dart';
 import 'package:public_e_commerce_api_dart/router/index.dart';
 
 void main(List<String> arguments) async {
-  // await DBProvider.db.connection;
-  //
-  // final palace = Palace();
-  // palace.use(BodyParser());
-  // Router(palace);
-  // await palace.openGates();
+  await DBProvider.db.connection;
 
-  final app = Alfred();
+  final palace = Palace();
+  palace.use(BodyParser());
+  Router(palace);
+  await palace.openGates();
 
-  app.get("/example", (req, res) => "Hello world");
-  app.get("/", (req, res) => "Hello world");
 
-  await app.listen();
 
   print("Listening on port 3000");
 }
